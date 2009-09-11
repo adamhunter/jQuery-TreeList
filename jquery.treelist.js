cd432af7ef95b16ce8441b09400ff3d6de85fc16
Adam Hunter <adamhunter@me.com>
2009-09-11 15:25:53 -0400

    removed some newlines updated to notate private methods by prefixing them with _

--- jquery.treelist.js
+++ jquery.treelist.js
@@ -1,14 +1,12 @@
 tl = {
   opts : {'expander' : '.expander', 'speed' : 'dev'},
-  
   treelist : function(opts) {
-    tl.setup(opts);
+    tl._setup(opts);
     tl.$tree = this;
     jQuery('li:not(.open) ul', tl.$tree).hide();
-    jQuery(tl.opts.expander, tl.$tree).click(tl.expand);
+    jQuery(tl.opts.expander, tl.$tree).click(tl._expand);
   },
-  
-  expand : function() {
+  _expand : function() {
     var $li = jQuery(this).closest('li');
     var $li_ul = jQuery('ul:first', $li)
     if ($li.hasClass('open')) {
@@ -20,13 +18,11 @@ tl = {
     }
     return false;
   },
-  
-  setup : function(opts) {
+  _setup : function(opts) {
     if (typeof opts == 'undefined') { return; }
     for (prop in opts) { 
        tl.opts[prop] = opts[prop];
     }
   }
 }
-
-jQuery.fn.extend(tl)
+jQuery.fn.extend(tl);
